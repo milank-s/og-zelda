11b64d003334c3bb94d267d62b524d0859f4eacc
Unknown <jackhterzi@gmail.com>
2017-11-06 11:35:14 -0500

    Spider movement Untuned
    the hop and movement is done just needs to be tuned to the gameplay. also need to add in collider by the walls

--- Assets/SpiderMovement.cs
+++ Assets/SpiderMovement.cs
@@ -6,31 +6,57 @@ public class SpiderMovement : MonoBehaviour {
     Rigidbody rb;
     public float minDistance;
     public float maxDistance;
-
+    public float minSpeed;
+    public float maxSpeed;
     float timer;
     public float timerMin;
     public float timerMax;
+    float moveTime;
+    Vector3 dir;
+    float force;
+    bool moveSet;
+    bool moving;
+    float jumpUp;
+    public float jumpHeight;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         timer = 1;
-	}
+        float moveTime = Random.Range(minDistance, maxDistance);
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (timer < 0) {
-
-            //direction is in 45 degree angles
-            //distance is random multiplyer 
-            //height is set
-            //
-            rb.MovePosition(transform.position+ new Vector3(transform.position.x+ Random.Range());
-
-
-
-
+            moveSet = true;
             timer = Random.Range(timerMin, timerMax);
+
         }
+        if (moveSet) {
+            dir = Random.insideUnitSphere;
+            force = Random.Range(minSpeed, maxSpeed);
+            moveTime = Random.Range(minDistance, maxDistance);
+            jumpUp = moveTime / 2;
+            moveSet = false;
+        }
+        if (moveTime > 0) {
+            moving = true;
+            if (moveTime > jumpUp) {
+                rb.MovePosition(transform.position + new Vector3(dir.x, 0, dir.z) * force * Time.deltaTime + new Vector3(0, jumpHeight * Time.deltaTime, 0));
+
+            }
+            else {
+                rb.MovePosition(transform.position + new Vector3(dir.x, 0, dir.z) * force * Time.deltaTime);
+            }
+
+            moveTime -= Time.deltaTime;
+        }
+        else {
+            timer -= Time.deltaTime;
+            
+        }
+       
+        
 	}
 
 
