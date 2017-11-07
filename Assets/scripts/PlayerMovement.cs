aaf7e275bd867cf26d31a8968f2b9064cb4fb2b4
Unknown <jg4656@nyu.edu>
2017-11-07 10:53:22 -0500

    Added Health Script and Attack in Player Movement function via Raycast

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public bool ShieldUp;
+	public GameObject Victim;
 
 	// Use this for initialization
 	void Start () {
@@ -27,14 +28,29 @@ public class PlayerMovement : MonoBehaviour {
 			transform.Translate (0f, 0f, 5f * Time.deltaTime);
 		} 
 
+		Ray Attack=new Ray(transform.position, transform.forward);
+
+		float maxRayDist = 1.0f;
+
+		Debug.DrawRay (Attack.origin, Attack.direction, Color.black, maxRayDist);
+
+		RaycastHit AttackHit = new RaycastHit ();
+
 		if (Input.GetKey (KeyCode.U)) {
 			ShieldUp = true;
 		} else if (Input.GetKeyUp (KeyCode.U)){
 			ShieldUp = false;
-		} else if(Input.GetKeyDown.O)){
+		} else if(Input.GetKeyDown(KeyCode.O)){
 			//will put sword attack script in here once health script and attack function are coded
+			if (Physics.Raycast (Attack, out AttackHit, maxRayDist)) {
+				Victim = AttackHit.transform.gameObject;
+				Victim.GetComponent<Health> ().Hit = true;
+			}
 		}
 
 
 	}
+	
+			
+
 }
