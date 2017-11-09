46f2e951f529339ad072ef2f6d8c94e3a62996a6
Milan Koerner-Safrata <mks554@nyu.edu>
2017-11-09 15:54:55 -0500

    comments
    read

--- Assets/scripts/qBertMovement.cs
+++ Assets/scripts/qBertMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class qBertMovement : MonoBehaviour {
 
+	//these variable names are very confusing. 
+	//maybe shootProbability, isTurning, turnProbability
+	
 	float directionChangeNumber;
 	float shootTurnNumber;
 	float likelyToShoot = 0.1f;
@@ -23,13 +26,18 @@ public class qBertMovement : MonoBehaviour {
 	void Update () {
 		shootTurnNumber = Random.Range (0f, 0.99f);
 
+		//Does it collide? don't use translate
+		
 		transform.Translate(Vector3.forward * Time.deltaTime);	
 
 
-
+		// find a way to clean up the probability with some public variables
+		//very hard to tune something like this
+		
 		if (shootTurnNumber <= likelyToShoot) {
 			//shoot!
 			Instantiate (bullet, transform.position, transform.rotation);
+			//might as well be 0.
 			likelyToShoot = 0.0002f;
 			directionChangeNumber = Random.Range (0f, 2.99f);
 			if (directionChangeNumber <= 0.99f) {
@@ -42,11 +50,16 @@ public class qBertMovement : MonoBehaviour {
 				transform.rotation = Quaternion.Euler (0, transform.rotation.y + 180, 0);
 			}
 		} else {
+			//use time.deltaTime so higher framerates won't make them shoot faster
+			
 			likelyToShoot += 0.0002f;
 		}
 	}
 
 	void OnCollisionEnter () {
+		
+		//if you're doing redirection code in both update and OnCollision, write a function for it
+		
 		//number to decide change of direction
 		directionChangeNumber = Random.Range (0f, 2.99f);
 
@@ -61,6 +74,10 @@ public class qBertMovement : MonoBehaviour {
 			//one third of the time turn completely around
 			transform.rotation = Quaternion.Euler (0, transform.rotation.y + 180, 0);
 		}
+		
+		//why is the probability set back to a higher value (0.1) here than in Update?
+		//make a function for this as well. Re-use the function
+		
 		if (shootTurnNumber <= likelyToShoot) {
 			//shoot!
 			Instantiate (bullet, transform.position, transform.rotation);
