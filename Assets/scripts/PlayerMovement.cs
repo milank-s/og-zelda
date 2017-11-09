768149af1b5a7d8ce6a8699ba40a08c7abec13d5
Unknown <jg4656@nyu.edu>
2017-11-09 13:48:49 -0500

    Fixing of the Character Control
    Bug in the sword position causing problems

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,6 +7,15 @@ public class PlayerMovement : MonoBehaviour {
 	public bool ShieldUp;
 	public GameObject Victim;
 
+	public GameObject Sword;
+	public bool HasSword=true;
+	public bool IsAttacking = false;
+
+	public Vector3 SwordOut;
+	public Vector3 SwordIn=new Vector3 (0,0,0);
+	public Vector3 moveDir;
+	public bool IsSwordIn=true;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -28,13 +37,12 @@ public class PlayerMovement : MonoBehaviour {
 			transform.Translate (0f, 0f, 5f * Time.deltaTime);
 		} 
 
-		Ray Attack=new Ray(transform.position, transform.forward);
+		Ray Direction=new Ray(transform.position, transform.forward);
 
 		float maxRayDist = 1.0f;
 
-		Debug.DrawRay (Attack.origin, Attack.direction, Color.black, maxRayDist);
+		Debug.DrawRay (Direction.origin, Direction.direction, Color.black, maxRayDist);
 
-		RaycastHit AttackHit = new RaycastHit ();
 
 		if (Input.GetKey (KeyCode.U)) {
 			ShieldUp = true;
@@ -42,15 +50,42 @@ public class PlayerMovement : MonoBehaviour {
 			ShieldUp = false;
 		} else if(Input.GetKeyDown(KeyCode.O)){
 			//will put sword attack script in here once health script and attack function are coded
-			if (Physics.Raycast (Attack, out AttackHit, maxRayDist)) {
-				Victim = AttackHit.transform.gameObject;
-				Victim.GetComponent<Health> ().Hit = true;
+			if (HasSword==true){
+				IsAttacking = true;
 			}
 		}
 
+		if (IsAttacking= true) {
+			if (IsSwordIn == true) {
+//				moveDir = SwordOut - SwordIn;
+//				if (moveDir.magnitude > .1f) {
+//					moveDir = Vector3.Normalize (moveDir);
+//					moveDir.z = moveDir.z * .1f;
+//				}
+//				Sword.transform.localPosition += moveDir * Time.deltaTime * 5.0f;
 
+				Sword.GetComponent<Transform> ().localPosition = SwordOut;
+				IsSwordIn = false;
+
+			} else if (IsSwordIn == false) {
+//				moveDir = -SwordOut + SwordIn;
+//				if (moveDir.magnitude > .1f) {
+//					moveDir = Vector3.Normalize (moveDir);
+//					moveDir.z = moveDir.z * .1f;
+//				}
+//				Sword.transform.localPosition += moveDir * Time.deltaTime * 5.0f;
+
+				Sword.GetComponent<Transform> ().localPosition = SwordIn;
+				IsSwordIn = true;
+			}
+
+			if (Sword.GetComponent<Transform> ().localPosition.z <= .01 || Sword.GetComponent<Transform> ().localPosition.z >= -.01) {
+				IsAttacking = false;
+			}
+		}
 	}
-	
+
+
 			
 
 }
